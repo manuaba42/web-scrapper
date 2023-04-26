2c3107887cf7b5a3d2b0b40b4f5081df3d28439b
manuaba42 <ssedanayoga3@gmail.com>
2023-04-26 22:50:06 +0800

    scrap

--- scrap2.py
+++ scrap2.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from tabulate import tabulate
 
 web = 'https://www.vivino.com/explore?e=eJzLLbI11rNQy83MszVQy02ssDU1MFBLrrQNDVZLBhIuagW2hmrpabZliUWZqSWJOWr5RSm2avlJlbZq5SXRsUBJMGUEoYwhlIlasW1yIgASQhzB'
-# path = '/usr/local/bin/chromedriver'
-# driver = webdriver.Chrome()
+
 s = Service('usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=s)
 
@@ -13,13 +13,23 @@ driver.get(web)
 
 
 items = driver.find_elements(By.CLASS_NAME, 'wineCard__wineCard--2dj2T')
-# items = driver.find_elements(By.XPATH, "//div[contains(@class, 'wineCard__wineCard--2dj2T')]")
-# print(items)
-for item in items:
-    name = item.find_element(By.CLASS_NAME,'wineInfoVintage__vintage--VvWlU').text
-    # name = item.find_element(By.XPATH, "//div[contains(@class, 'wineInfoVintage__truncate--3QAtw')]").text
-    # item.find_element(By.XPATH("//div[@class='notesData']/div[@class='notesDate']")).getText()
-    print(name)
 
-while True:
-    continue
+wines = [['Name', 'Region', 'Rating']]
+for item in items:
+    wine = []
+
+    name = (item.find_element(By.CLASS_NAME,'wineInfoVintage__vintage--VvWlU').text)
+    region = (item.find_element(By.CLASS_NAME,'wineInfoLocation__regionAndCountry--1nEJz').text)
+    # prices = item.find_element(By.CLASS_NAME,'addToCartButton__price--qJdh4')
+    # for price in prices:
+    #     print(price)
+    rating = (item.find_element(By.CLASS_NAME,'vivinoRating_averageValue__uDdPM').text)
+
+    wine = [name, region, rating]
+
+    wines.append(wine)
+
+print(tabulate(wines))
+
+# while True:
+#     continue
